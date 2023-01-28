4c355e2c3296ec8dbc9d7308307bddf86974da97
DarianGrabino <dariangra@gmail.com>
2023-01-28 12:40:30 -0800

    deletes a key in a dictionary

---
+++ python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    if (key in a_dictionary)
+        a_dictionary.pop(key)
+    return a_dictionary
+# pop() elimina y retorna un elemento de una lista,
+# especificar índice a ser eliminado de la lista,
+# si no se especifica ningún índice
+# elimina y retorna el último elemento de la lista
