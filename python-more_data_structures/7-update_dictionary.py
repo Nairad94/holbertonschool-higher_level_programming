1f2645e6a06e722946a28f5e34a73be9cb6cbbb1
DarianGrabino <dariangra@gmail.com>
2023-01-28 11:59:27 -0800

    eplaces or adds key/value in a dictionary

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary.update({key: value})
+    return a_dictionary
+# update() actualiza el diccionario con los elementos de
+# otro objeto de diccionario o de una iteración de pares clave/valor
