b8bd034e54975d432481214874eae7fec06de18c
DarianGrabino <dariangra@gmail.com>
2023-01-31 06:05:27 -0800

    Access and update private attribute

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -3,17 +3,18 @@
 
 
 class Square:
+    """ Private instance attribute: size """
     def __init__(self, __size=0):
         self.__size = __size
-
+    """ area of a square """
     def area(self):
         area = self.__size * self.__size
         return area
-
+    """ Devolver un atributo de propiedad """
     @property
     def size(self):
         return self.__size
-
+    """ permite establecer o mutar el valor de un atributo en una clase """
     @size.setter
     def size(self, value):
         if (type(value)) != int:
