2ef642ea7452dedd6e01a28d3ea98ca5f357a17e
DarianGrabino <dariangra@gmail.com>
2023-02-03 04:47:20 -0800

    Say my name

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Write a function that prints My name """
+
+
+def say_my_name(first_name, last_name=""):
+    if type(first_name) != str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) != str:
+        raise TypeError("last_name must be a string")
+    else:
+        print("My name is ", end="")
+        for fn in range(len(first_name)):
+            print(first_name[fn], end="")
+        print(" ", end="")
+        for ln in range(len(last_name)):
+            print(last_name[ln], end="")
+        print()
