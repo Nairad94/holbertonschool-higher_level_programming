9ae5790b85bdaf541be7e5d859e98122e2b08ab2
DarianGrabino <dariangra@gmail.com>
2023-02-03 10:00:30 -0800

    say my name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -12,7 +12,7 @@ def say_my_name(first_name, last_name=""):
         print("My name is ", end="")
         for fn in range(len(first_name)):
             print(first_name[fn], end="")
-        print("", end="")
+        print(" ", end="")
         for ln in range(len(last_name)):
             print(last_name[ln], end="")
         print()
