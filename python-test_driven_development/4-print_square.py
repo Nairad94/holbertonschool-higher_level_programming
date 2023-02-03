0a690567a74e6ca675c07ef617c1fb51e02cb49e
DarianGrabino <dariangra@gmail.com>
2023-02-03 09:02:30 -0800

    print square

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -3,12 +3,13 @@
 
 
 def print_square(size):
+    """ Print square with the character # """
     if (type(size)) != int:
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
     else:
-        for x in range (size):
-            for y in range (size -1):
+        for x in range(size):
+            for y in range(size-1):
                 print("#", end="")
             print("#")
