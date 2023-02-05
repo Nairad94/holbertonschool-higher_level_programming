5700ae81ffa97262a60959ce217fe3c055733c32
DarianGrabino <dariangra@gmail.com>
2023-02-05 15:47:41 -0800

    text indentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -3,12 +3,16 @@
 
 
 def text_indentation(text):
+    """ prints a text with 2 new lines after each of these char: .?: """
     if (type(text)) != str:
-            raise TypeError("text must be a string")
-    else:
-        for char in text:
-            if (char == '.' or char == '?' or char == ':'):
-                print(char)
-                print()
-            else: 
-                print(char, end="")
+        raise TypeError("text must be a string")
+    text1 = ".".join([substring.strip() for substring in text.split(".")])
+    text2 = ":".join([substring.strip() for substring in text1.split(":")])
+    text3 = "?".join([substring.strip() for substring in text2.split("?")])
+
+    for char in text:
+        if (char == '.' or char == '?' or char == ':'):
+            print(char)
+            print()
+        else:
+            print(char, end="")
