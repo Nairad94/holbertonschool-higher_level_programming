b98956ef04b8c989d02ff3d180ac32a21d232871
DarianGrabino <dariangra@gmail.com>
2023-02-07 07:10:01 -0800

    My List

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ class MyList that inherits from list """
+class MyList(list):
 
 
-def print_sorted(self):
-    """ that prints the list sorted """
-    class my_list(list):
-        pass
+    def print_sorted(self):
+        """ that prints the list sorted """
+        print(sorted(self))
