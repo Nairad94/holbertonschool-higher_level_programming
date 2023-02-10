b633ba8bd134ac7d4065cf546dd4111d01811750
DarianGrabino <dariangra@gmail.com>
2023-02-10 08:45:37 -0800

    Append to a file

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" function that appends a string at the end of a text file """
+
+
+def append_write(filename="", text=""):
+    with open (filename, "a", encoding="utf-8") as file:
+        file.write(text)
+        return len(text)
