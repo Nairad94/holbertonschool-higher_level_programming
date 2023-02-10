6895060922cbaa10c6e0a4f4632cb30460be795c
DarianGrabino <dariangra@gmail.com>
2023-02-10 06:28:35 -0800

    Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-""" function that reads a text file (UTF8) and prints it to stdout """
+""" function that read file """
 
 
 def read_file(filename=""):
+    """ reads a text file (UTF8) and prints it to stdout """
     with open(filename, encoding="utf-8") as file:
         read_file = file.read()
         print(read_file)
