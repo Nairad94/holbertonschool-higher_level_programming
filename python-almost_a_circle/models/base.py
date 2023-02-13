5b7a3309552c2f288eabbc8415dfa9411aac1527
DarianGrabino <dariangra@gmail.com>
2023-02-13 08:44:44 -0800

    Base class

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Define the start of the class """
+
+
+class Base:
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ defines a private class attribute """
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
