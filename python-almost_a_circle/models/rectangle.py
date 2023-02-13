7145c083fcba14cf21a52894c1bb2bbd62941a4a
DarianGrabino <dariangra@gmail.com>
2023-02-13 11:58:26 -0800

    First Rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -6,48 +6,48 @@ from models.base import Base
 class Rectangle(Base):
     """defines an initialization method """
     def __init__(self, width, height, x=0, y=0, id=None):
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
     @property
-    def __width(self):
+    def width(self):
         """ getter """
         return self.__width
 
     @property
-    def __height(self):
+    def height(self):
         """ getter """
         return self.__height
 
     @property
-    def __x(self):
+    def x(self):
         """ getter """
         return self.__x
 
     @property
-    def __y(self):
+    def y(self):
         """ getter """
         return self.__y
 
-    @__height.setter
+    @height.setter
     def height(self, value):
         """ setter """
         self.height = value
 
-    @__width.setter
+    @width.setter
     def width(self, value):
         """ setter """
         self.width = value
 
-    @__x.setter
+    @x.setter
     def x(self, value):
         """ setter """
         self.x = value
 
-    @__y.setter
+    @y.setter
     def y(self, value):
         """ setter """
         self.y = value
