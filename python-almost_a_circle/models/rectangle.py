5ed4d924b6932544e00e34d9581645f2c5cb402e
DarianGrabino <dariangra@gmail.com>
2023-02-16 16:32:35 -0800

    update#2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -4,7 +4,7 @@ from models.base import Base
 
 
 class Rectangle(Base):
-    """defines an initialization method """
+    """ defines an initialization method """
     def __init__(self, width, height, x=0, y=0, id=None):
         self.width = width
         self.height = height
