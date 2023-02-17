4dfe38e1b01df1772e1eaedd32cabe2869c36a41
DarianGrabino <dariangra@gmail.com>
2023-02-17 15:06:44 -0800

    Square update

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -41,4 +41,15 @@ class Square(Rectangle):
             print()
 
     def update(self, *args, **kwargs):
-        return super().update(*args, **kwargs)
+        """ update argument order """
+        if len(args) > 1:
+            self.id = args[0]
+        if len(args) > 2:
+            self.size = args[1]
+        if len(args) > 3:
+            self.x = args[2]
+        if len(args) > 4:
+            self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
