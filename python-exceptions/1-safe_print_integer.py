a2d8d02132aac12c4d6a274ff71bc83412945855
DarianGrabino <dariangra@gmail.com>
2023-03-14 17:29:15 -0700

    pycodestyle

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """printing of an integers list"""
 
+
 def safe_print_integer(value):
     """function that print integer or return false"""
     try:
-        print ("{:d}".format(value))
-    except(ValueError, TypeError):
+        print("{:d}".format(value))
+    except (ValueError, TypeError):
         return False
     else:
         return True
