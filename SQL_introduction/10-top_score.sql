0a1cebe01650cf9b133d28934f7fcf127697b4cc
DarianGrabino <dariangra@gmail.com>
2023-03-16 14:33:53 -0700

    score and name in order descending

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- selects the score and name fields from the second_table of the hbtn_0c_0 database and sorts them in descending order
+SELECT score, name
+FROM hbtn_0c_0.second_table
+ORDER BY score DEC;
