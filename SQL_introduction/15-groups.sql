ae4c3b06e0097512a0899aaf791ae280f48b8629
DarianGrabino <dariangra@gmail.com>
2023-03-16 16:59:44 -0700

    lists all records of the table

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- lists the number of records with the same score in the table second_table, returns a list of results with two columns: "score" and "number"
-SELECT score, COUNT(score) as number FROM second_table GROUP BY score ORDER BY score;
+SELECT score, COUNT(score) as number FROM second_table GROUP BY score ORDER BY score DESC;
