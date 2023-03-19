5ac457d3b149bcca559140cb4a0c92e56e7a7103
DarianGrabino <dariangra@gmail.com>
2023-03-19 09:42:53 -0700

    All cities by state

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,9 +6,9 @@ import MySQLdb
 
 def select_states():
     """ lists all states from the database hbtn_0e_0_usa"""
-    database = MySQLdb.connect(host='localhost', user=argv[1],
-                         passwd=argv[2], database=argv[3])
-    cur = database.cursor()
+    db = MySQLdb.connect(host='localhost', user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
     cur.execute("SELECT * FROM states")
     rows = cur.fetchall()
     for row in rows:
