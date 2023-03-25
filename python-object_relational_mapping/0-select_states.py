fe91af3112b2c90d905487ae9526f81d0b0608ba
DarianGrabino <dariangra@gmail.com>
2023-03-25 10:48:22 -0700

    lists all states from the database hbtn_0e_0_usa

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+from sys import argv
+import MySQLdb
+
+def lists_states():
+    """ lists all states from the detabase """
+    
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        password=argv[2],
+        db=argv[3]
+    )
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states")
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
+
+if __name__ == "__main__":
+    lists_states()
