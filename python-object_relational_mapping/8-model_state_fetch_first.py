3393ed91bd99fab4dcc5799368ea2b64c31c449f
DarianGrabino <dariangra@gmail.com>
2023-03-26 11:49:06 -0700

    first state

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""All states via SQLAlchemy"""
+import sys
+from sqlalchemy import create_engine
+from model_state import Base, State
+
+
+def lists_states():
+    """print first state"""
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db = sys.argv[3]
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(user, password, db)
+        )
+    connection = engine.connect()
+    result = connection.execute('SELECT * FROM states LIMIT 1')
+    for elem in result:
+        print(f"{elem[0]}: {elem[1]}")
+
+
+if __name__ == '__main__':
+    lists_states()
