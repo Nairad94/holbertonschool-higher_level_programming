20a5d89f625c05b0f312cdeb8c289ef6dfeb2d45
DarianGrabino <dariangra@gmail.com>
2023-03-25 18:18:37 -0700

    Cities by states

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Cities by states"""
+from sys import argv
+import MySQLdb
+
+
+def lists_states():
+    """ lists all cities from the database """
+
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        password=argv[2],
+        db=argv[3]
+    )
+
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT ROW_NUMBER() OVER(), cities.name,states.name \
+        FROM cities, states WHERE state_id = states.id"
+    )
+    result = cursor.fetchall()
+    for row in result:
+            print(row)
+
+
+if __name__ == "__main__":
+    lists_states()
