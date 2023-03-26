8810af6d1081ce9e184cdd2de8e95fb4ad690bd9
DarianGrabino <dariangra@gmail.com>
2023-03-26 13:01:58 -0700

    Contains , fix %a%

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -15,7 +15,7 @@ def lists_states():
         'mysql+mysqldb://{}:{}@localhost/{}'.format(user, password, db)
         )
     session = Session(engine)
-    states = session.query(State).filter(State.name.like('%a')).all()
+    states = session.query(State).filter(State.name.like('%a%')).all()
 
     for state in states:
         print(f"{state.id}: {state.name}")
