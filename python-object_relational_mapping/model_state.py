8088504bd04c10c6f493f451f07f3570a6a6b051
DarianGrabino <dariangra@gmail.com>
2023-03-26 17:05:40 -0700

    add foreign key relationship

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-"""First State model"""
+"""State model"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
+
 
 
 Base = declarative_base()
@@ -12,3 +15,11 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
+
+class City(Base):
+    """ class for the cities table"""
+    __tablename__ = 'cities'
+    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+    state = relationship("State", back_populates="cities")
