574d41cdec9abff36d4fd043bf1f3b116d3e19bb
DarianGrabino <dariangra@gmail.com>
2023-03-26 17:11:10 -0700

    fix pycodestyle, model city

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -4,7 +4,9 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
+
 Base = declarative_base()
+
 
 class City(Base):
     """ class for the cities """
